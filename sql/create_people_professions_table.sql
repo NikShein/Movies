0bbfc82e2a823a37a39f721cf31a8bc04ec4f9df
NikShein <nikshein93@gmail.com>
2022-02-18 18:26:51 +0200

    DB-7 update create scripts, add ref to people_prof

--- sql/create_people_professions_table.sql
+++ sql/create_people_professions_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE people_professions 
 (
   id SERIAL PRIMARY KEY,
-  people_id INTEGER REFERENCES people (id),
-  professions_id INTEGER REFERENCES professions (id)
+  people_id INTEGER REFERENCES people (id) ON DELETE CASCADE,
+  professions_id INTEGER REFERENCES professions (id) ON DELETE CASCADE
 )
